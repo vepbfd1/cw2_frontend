b785e2701fb48e9d92a01b648c7f9138d50a95ae
vepbfd1 <166491824+vepbfd1@users.noreply.github.com>
2024-06-18 21:50:39 +0800

    updated new version

---
+++ src/components/common/http-common.ts
@@ -0,0 +1,7 @@
+export const api = {
+  uri:"http://localhost:10888/api/v1"
+}
+//https://feec368b-f2cd-4c78-ba6d-d66f03597562-00-2xj5l7c94qwox.sisko.repl.co:3000/
+//5180bb11-29ac-4f58-97b1-2b025e251db8-00-3415vnygwmulx.sisko.replit.dev/
+
+//90b48509-8be0-43fd-aa07-b3855da4015a-00-2dafu0mps44fp.pike.replit.dev/
